2cd5efbe76a80f74411d370548f34af1f3657703
Matteo Ferrabone <matteo.ferrabone@gmail.com>
2020-11-01 17:28:13 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()    
 
 setuptools.setup(
     name="peepy", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Matteo Ferrabone",
     author_email="matteo.ferrabone@gmail.com",
+    license='MIT',
     description="A simple Observer Pattern implementation with decorators",
     long_description=long_description,
     long_description_content_type="text/markdown",
